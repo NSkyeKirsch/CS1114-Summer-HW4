69b9443033fcd69ca9c7b0325fd2cfcdc26717b6
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-06-15 19:17:58 -0400

    added life.txt

---
+++ hw4_p1.py
@@ -0,0 +1,46 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW4 - Q1
+Date due: 2022-06-21
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+def initialize_world(in_filepath):
+    # use error handling to deal with case where there is no input file
+    # if error, return an empty world and do not allow anything else to happen
+
+def count_neighbors(world, position):
+    # position is a row and column
+    # count number of living neighbors next to the position
+
+def compute_next_generation(start_generation):
+
+
+
+def display_world(world):
+    # print representation of world to the screen, looks like input file
+
+
+
+def main():
+    # print original and then 10 generations after
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
